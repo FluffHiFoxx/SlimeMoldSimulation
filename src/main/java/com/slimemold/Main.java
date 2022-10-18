6d17899e0c7ae038dab1de0339dabafa91fb4fcb
M9Bene <bene9marci@gmail.com>
2022-10-18 11:23:26 +0200

    Add: Test for Grid Pane

--- src/main/java/com/slimemold/Main.java
+++ src/main/java/com/slimemold/Main.java
@@ -5,6 +5,9 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -26,8 +29,26 @@ public class Main extends Application {
         this.graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
         Scene scene = new Scene(this.window);
         this.window.setCenter(this.canvas);
-        stage.setScene(scene);
-        stage.setTitle("Simulation");
+/*
+        GridPane root = new GridPane();
+        root.setGridLinesVisible(true);
+        final double numCols = canvas.getWidth();
+        final double numRows =  canvas.getHeight();
+        for (int i = 0; i < numCols; i++) {
+            ColumnConstraints colConst = new ColumnConstraints();
+          //  colConst.setPercentWidth(100.0 / numCols);
+            colConst.setPrefWidth(numCols);
+            root.getColumnConstraints().add(colConst);
+        }
+        for (int i = 0; i < numRows; i++) {
+            RowConstraints rowConst = new RowConstraints();
+            //rowConst.setPercentHeight(100.0 / numRows);
+            rowConst.setPrefHeight(numRows);
+            root.getRowConstraints().add(rowConst);
+        }
+        stage.setScene(new Scene(root, 800, 600));
         stage.show();
+        */
+
     }
 }
