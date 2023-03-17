bec44b773dc05eccdef4154ac57f1a9de5b1e011
M9Bene <bene9marci@gmail.com>
2023-03-17 13:47:32 +0100

    Refactor: LiveCell movement

--- src/main/java/com/slimemold/board/LiveCell.java
+++ src/main/java/com/slimemold/board/LiveCell.java
@@ -5,14 +5,25 @@ import javafx.scene.paint.Color;
 import java.util.Arrays;
 
 public class LiveCell extends Cell {
-    private int[] movesLeft;
+
+    private int[] firstMove;
+
+    private int[] secondMove;
+
+    private int stepCounter;
+
 
     public LiveCell(Color color, int x, int y) {
         super(color, x, y);
-        this.movesLeft = direction;
+        this.firstMove = new int[2];
+        this.secondMove = new int[2];
+        // setting first move and second move
+        calculateMoves(this.direction);
+        // set step counter
+        this.stepCounter = 1;
     }
 
-    public LiveCell(int[] direction, Color color, int x, int y) {
+    public LiveCell(Color color, int x, int y, int[] direction) {
         super(color, x, y);
         if (Arrays.equals(direction, new int[]{0, 0})) {
             direction = getRandomDirection();
@@ -24,60 +35,80 @@ public class LiveCell extends Cell {
             direction[1] = 2 * (Math.abs(direction[0]) / direction[1]);
         }
         this.direction = direction;
-        this.movesLeft = direction;
+
+        this.firstMove = new int[2];
+        this.secondMove = new int[2];
+
+        calculateMoves(direction);
+        this.stepCounter = 1;
     }
 
     public void move(Board board) {
-        int[] difference = moveToMake();
-        int originalX = xCoordinate;
-        int originalY = yCoordinate;
-        int nextX = xCoordinate + difference[0];
-        int nextY = yCoordinate + difference[1];
-        int maxX = board.getWidth() - 1;
-        int maxY = board.getHeight() - 1;
-        if (nextX >= 1 && nextX < maxX) {
-            setxCoordinate(nextX);
-        } else {
-            setxCoordinate(xCoordinate + bounceOff(0));
-        }
-        if (nextY >= 1 && nextY < maxY) {
-            setyCoordinate(nextY);
-        } else {
-            setyCoordinate(yCoordinate + bounceOff(1));
-        }
-        Trail trail = new Trail(getColor(), originalX, originalY);
-        board.setCell(originalY, originalX, trail);
+
+        Trail trail = new Trail(this.getColor(), getxCoordinate(),
+                getyCoordinate(), this.direction);
+        board.setCell(getyCoordinate(), getxCoordinate(), trail);
         board.addTrail(trail);
-    }
 
-    private int[] moveToMake() {
-        if (Arrays.equals(movesLeft, new int[]{0, 0})) {
-            movesLeft = direction;
-        }
-        int x = Math.abs(movesLeft[0]);
-        int y = Math.abs(movesLeft[1]);
-        int[] firstMove;
-        int[] secondMove;
-        if (x < y) {
-            secondMove = new int[]{x - 1, y > 0 ? y - 1 : y};
-        } else if (y < x) {
-            secondMove = new int[]{x > 0 ? x - 1 : x, y - 1};
+        if (stepCounter == 1) {
+            this.setxCoordinate(getxCoordinate() + firstMove[0]);
+            this.setyCoordinate(getyCoordinate() + firstMove[1]);
+
+            this.stepCounter += 1;
+
         } else {
-            secondMove = new int[]{x > 0 ? x - 1 : x, y > 0 ? y - 1 : y};
+            this.setxCoordinate(getxCoordinate() + secondMove[0]);
+            this.setyCoordinate(getyCoordinate() + secondMove[1]);
+
+            this.stepCounter -= 1;
         }
-        secondMove[0] = x == 0 ? 0 : (secondMove[0] * (x / movesLeft[0]));
-        secondMove[1] = y == 0 ? 0 : (secondMove[1] * (y / movesLeft[1]));
-        firstMove = new int[]{movesLeft[0] - secondMove[0], movesLeft[1] - secondMove[1]};
-        movesLeft = secondMove;
-        return firstMove;
+
+        if (getxCoordinate() >= 0 && getxCoordinate() < board.getWidth() &&
+                getyCoordinate() >= 0 && getyCoordinate() < board.getHeight()) {
+
+            board.setCell(getyCoordinate(), getxCoordinate(), this);
+        } else {
+            // change direction
+            if (getxCoordinate() < 0) {
+                setxCoordinate(0);
+                direction[0] = -direction[0];
+            } else if (getxCoordinate() >= board.getWidth() - 1) {
+                setxCoordinate(board.getWidth() -1 );
+                direction[0] = -direction[0];
+            }
+
+            if (getyCoordinate() < 0) {
+                setyCoordinate(0);
+                direction[1] = -direction[1];
+            } else if (getyCoordinate() >= board.getHeight() - 1) {
+                setyCoordinate(board.getHeight() - 1);
+                direction[1] = -direction[1];
+            }
+
+            calculateMoves(this.direction);
+            board.setCell(getyCoordinate(), getxCoordinate(), this);
+        }
     }
 
-    public int bounceOff(int i) {
-        direction[i] = -direction[i];
-        return moveToMake()[i];
-    }
 
     public void changeDirection(int[] direction) {
         this.direction = direction;
     }
+
+    private void calculateMoves(int[] direction) {
+        if (Math.abs(direction[0]) > 1) {
+            firstMove[0] = direction[0] / 2;
+        } else {
+            firstMove[0] = direction[0];
+        }
+        if (Math.abs(direction[0]) > 1) {
+            firstMove[1] = direction[1] / 2;
+        } else {
+            firstMove[1] = direction[1];
+        }
+        // setting second move
+        this.secondMove[0] = direction[0] - this.firstMove[0];
+        this.secondMove[1] = direction[1] - this.firstMove[1];
+    }
+
 }
