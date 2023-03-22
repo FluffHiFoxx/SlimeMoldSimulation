4e95d2d4f90ca3edff58a9c30cc59eeab1997e90
FluffHiFoxx <hakos2k@gmail.com>
2023-03-22 10:51:36 +0100

    Refactor: Trail addition changes and Board now contains only trails

--- src/main/java/com/slimemold/board/Board.java
+++ src/main/java/com/slimemold/board/Board.java
@@ -1,11 +1,13 @@
 package com.slimemold.board;
 
+import javafx.scene.paint.Color;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class Board {
     private final Set<Cell> CELLS;
-    private final Cell[][] BOARD;
+    private final Trail[][] BOARD;
     private final int WIDTH;
     private final int HEIGHT;
 
@@ -13,7 +15,7 @@ public class Board {
         this.CELLS = new HashSet<>();
         this.WIDTH = width;
         this.HEIGHT = height;
-        this.BOARD = new Cell[height][width];
+        this.BOARD = new Trail[height][width];
     }
 
     public void handleCells() {
@@ -32,11 +34,26 @@ public class Board {
     }
 
     public void addTrail(Trail trail) {
-        CELLS.add(trail);
+        if (BOARD[trail.yCoordinate][trail.xCoordinate] != null) {
+            Color trailColor = trail.getColor();
+            Color boardColor = BOARD[trail.yCoordinate][trail.xCoordinate].getColor();
+            CELLS.remove(BOARD[trail.yCoordinate][trail.xCoordinate]);
+            double r = trailColor.getRed() + boardColor.getRed();
+            double g = trailColor.getGreen() + boardColor.getGreen();
+            double b = trailColor.getBlue() + boardColor.getBlue();
+            Color color = Color.color(Math.min(1,r), Math.min(1,g), Math.min(1,b));
+            Trail newTrail = new Trail(color, trail.xCoordinate, trail.yCoordinate, trail.getDirection());
+            BOARD[trail.yCoordinate][trail.xCoordinate] = newTrail;
+            CELLS.add(newTrail);
+        } else {
+            CELLS.add(trail);
+            BOARD[trail.yCoordinate][trail.xCoordinate] = trail;
+        }
     }
 
     public void removeTrail(Trail trail) {
         CELLS.remove(trail);
+        BOARD[trail.yCoordinate][trail.xCoordinate] = null;
     }
 
     public int getWidth() {
